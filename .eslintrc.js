94340c5d72dae5919fe92835bb1adb0670b7d3cf
Matheus Pedroni <mpedroni77@gmail.com>
2021-03-19 14:05:36 -0300

    removed eslint rule "class-methods-use-this"

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'prettier/prettier': 'warn',
+    'class-methods-use-this': 'off'
   },
 };
