0c731bea048ca5cd488347a5667b6fa4e5488e14
Matheus Pedroni <mpedroni77@gmail.com>
2021-03-19 14:06:48 -0300

    created view main wrapper (TheContainer)

---
+++ src/components/TheContainer.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+    <slot />
+  </v-container>
+</template>
+
+<script>
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  name: 'the-container',
+})
+export default class TheContainer extends Vue {}
+</script>
