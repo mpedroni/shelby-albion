8ad1ce763d9d058a521808d65d473193015118e6
mpedroni <mpedroni77@gmail.com>
2021-03-22 11:21:54 -0300

    added link to project's github page

--- src/components/TheAppBar.vue
+++ src/components/TheAppBar.vue
@@ -2,6 +2,12 @@
   <v-app-bar app elevation="0" color="transparent">
     <v-icon color="primary">mdi-box-cutter</v-icon>
     <span class="primary--text font-weight-bold"> shelby </span>
+
+    <v-spacer />
+
+    <v-btn href="https://github.com/mpedroni/shelby-albion" icon large target="_blank">
+      <v-icon> mdi-github </v-icon>
+    </v-btn>
   </v-app-bar>
 </template>
 
