b53c58f8f92a7c82daeebb8695e59a83f0cb7048
Matheus Pedroni <mpedroni77@gmail.com>
2021-03-25 17:17:26 -0300

    removed requests interceptor (API)

--- src/plugins/api.ts
+++ src/plugins/api.ts
@@ -4,13 +4,4 @@ const server = axios.create({
   baseURL: 'http://localhost:3000',
 });
 
-server.interceptors.request.use((config) => {
-  if (!config.url?.includes('albion')) return config;
-
-  return {
-    ...config,
-    url: `https://albiononline2d.ams3.cdn.digitaloceanspaces.com/thumbnails/orig/${config.params.id}`,
-  };
-});
-
 export default server;
